2d0e6083a7e6da402c4f0f80b72df55b41747de9
user <user@WINDOWS-4PKKDM6>
2017-10-06 13:11:53 +0100

    First version of Kata1

---
+++ src/kata2/Kata2.java
@@ -0,0 +1,16 @@
+package kata2;
+
+/**
+ *
+ * @author user
+ */
+public class Kata2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
